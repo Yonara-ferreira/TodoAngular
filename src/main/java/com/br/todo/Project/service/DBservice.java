78e0e08583aa29940238badf8ac95b7e3083ce91
Yonara Ferreira <104727934+Yonara-ferreira@users.noreply.github.com>
2024-02-19 16:15:27 -0300

    Ajusted

--- src/main/java/com/br/todo/Project/service/DBservice.java
+++ src/main/java/com/br/todo/Project/service/DBservice.java
@@ -7,10 +7,7 @@ import org.springframework.stereotype.Service;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
-import java.util.Date;
 
 @Service
 public class DBservice {
@@ -20,11 +17,14 @@ public class DBservice {
 
     public void instantiateDatabase() throws ParseException {
         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-        Todo t1 = new Todo(null, "estudar", "Estudar Spring/Angular", sdf.parse("31/01/2024"), false);
+
+        Todo t1 = new Todo(null, "estudar", "Estudar Spring/Angular", sdf.parse("25/03/2022"), false);
         Todo t2 = new Todo(null, "Academia", "Treino superiores", sdf.parse("02/02/2024"), true);
         Todo t3 = new Todo(null, "netflix", "Assistir Ben 10", sdf.parse("02/02/2024 "), true);
         Todo t4 = new Todo(null, "Desenhar", "Fazer quadro do Zuko", sdf.parse("02/02/2024"), false);
-        todoRepository.saveAll(Arrays.asList(t1,t2,t3,t4));
+        Todo t5 = new Todo(null, "Correr", "Correr 20 kilometros ", sdf.parse("27/09/2021"), false);
+        Todo t6 = new Todo(null, "Trabalho", "Trabalho da faculdade para apresentar", sdf.parse("27/05/2021"), false);
+        todoRepository.saveAll(Arrays.asList(t1,t2,t3,t4,t5,t6));
     }
 
 
